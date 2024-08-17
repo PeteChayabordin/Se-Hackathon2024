ecdb24368cb55d0ad471801b7f365f2bb2a7e3c5
Your Name <6631503013@lamduan.mfu.ac.th>
2024-08-17 15:58:37 +0700

    hj

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World Min')
 })
+<<<<<<< HEAD
 
 app.get('/Hello', function (req, res) {
     res.send('Hello World Khant')
   })
 
+=======
+app.get('/', function (req, res) {
+  res.send('Users')
+})
+>>>>>>> 78ddd66 (hj)
 app.listen(3000)
