c12d619c7a24b112d9271ad4082e75c35db1bbaa
Min_Naing <6531503152@lamduan.mfu.ac.th>
2024-08-17 15:51:24 +0700

    hi

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World Min')
 })
+
+app.get('/Hello', function (req, res) {
+    res.send('Hello World Khant')
+  })
 
 app.listen(3000)
